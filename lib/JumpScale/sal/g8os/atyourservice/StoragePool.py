c86cd65c0092ab54cd8f8db987a6002fe111499d
serboctor <boctor.sarah@gmail.com>
2017-05-11 08:04:53 +0200

    8.2.0 disk status (#890)
    * Implement disk status for storagepool disks.
    
    * Implement disk status for storagepool disks.
    
    * Add a montior function to monitor disks and pool status.
    
    * Remove storagepool monitor function.

--- lib/JumpScale/sal/g8os/atyourservice/StoragePool.py
+++ lib/JumpScale/sal/g8os/atyourservice/StoragePool.py
@@ -15,25 +15,7 @@ class StoragePoolAys(AYSable):
         except j.exceptions.NotFound:
             service = None
 
-        device_map = []
-        disks = self._client.disk.list()['blockdevices']
-        for device in self._obj.devices:
-            info = None
-            for disk in disks:
-                if device == "/dev/%s" % disk['kname'] and disk['mountpoint']:
-                    info = disk
-                    break
-                for part in disk.get('children', []) or []:
-                    if device == "/dev/%s" % part['kname']:
-                        info = part
-                        break
-                if info:
-                    break
-
-            device_map.append({
-                'device': device,
-                'partUUID': info['partuuid'] or '' if info else '',
-            })
+        device_map, pool_status = self.get_devices_and_status()
 
         if service is None:
             # create new service
@@ -44,6 +26,7 @@ class StoragePoolAys(AYSable):
                 'dataProfile': self._obj.fsinfo['data']['profile'],
                 'devices': device_map,
                 'node': self._node_name,
+                'status': pool_status,
             }
             service = actor.serviceCreate(instance=self._obj.name, args=args)
         else:
@@ -52,9 +35,46 @@ class StoragePoolAys(AYSable):
             for i, device in enumerate(device_map):
                 service.model.data.devices[i] = device
 
+            service.model.data.status = pool_status
             service.saveAll()
 
         return service
+
+    def get_devices_and_status(self):
+        device_map = []
+        disks = self._client.disk.list()['blockdevices']
+        pool_status = 'healthy'
+        for device in self._obj.devices:
+            info = None
+            for disk in disks:
+                disk_name = "/dev/%s" % disk['kname']
+                if device ==  disk_name and disk['mountpoint']:
+                    info = disk
+                    break
+                for part in disk.get('children', []) or []:
+                    if device == "/dev/%s" % part['kname']:
+                        info = part
+                        break
+                if info:
+                    break
+
+            result = self._client.bash("smartctl -H %s > /dev/null ;echo $?" % disk_name).get()
+            exit_status = int(result.stdout)
+            status = 'healthy'
+
+            if exit_status & 1 << 0:
+                raise RuntimeError("Unable to get disk %s status" % disk_name)
+            if (exit_status & 1 << 2) or (exit_status & 1 << 3):
+                status = 'degraded'
+                pool_status = 'degraded'
+
+            device_map.append({
+                'device': device,
+                'partUUID': info['partuuid'] or '' if info else '',
+                'status': status,
+            })
+
+        return device_map, pool_status
 
     @property
     def _node_name(self):
